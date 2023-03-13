272af4c9f0f33ace6214fe3e042303fbffe1bf34
Peter Weiser <peterweiser87@gmail.com>
2023-03-14 00:09:17 +0200

    added eslint and prettier plus rules

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
-  console.log(StatusBar.currentHeight)
+  console.log(StatusBar.currentHeight);
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <View style={styles.search} >
+        <View style={styles.search}>
           <Text>Search</Text>
         </View>
         <View style={styles.list}>
           <Text>List</Text>
         </View>
       </SafeAreaView>
-      <ExpoStatusBar style='auto' />
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
@@ -21,15 +29,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   search: {
     padding: 16,
-    backgroundColor: 'green'
+    backgroundColor: "green",
   },
   list: {
     flex: 1,
     padding: 16,
-    backgroundColor: 'blue'
-  }
+    backgroundColor: "blue",
+  },
 });
