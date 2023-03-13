ed813ebeeefee88921b98eaa4ad04eeca521f18b
Peter Weiser <peterweiser87@gmail.com>
2023-03-13 22:58:14 +0200

    base layout added

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StyleSheet, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 export default function App() {
+  console.log(StatusBar.currentHeight)
   return (
-    <View>
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search} >
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto' />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  search: {
+    padding: 16,
+    backgroundColor: 'green'
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: 'blue'
+  }
 });
