ca48f401642a529ed3be7e1c9eadbe465e50d578
Peter Weiser <peterweiser87@gmail.com>
2023-03-14 01:20:16 +0200

    added Card with mockdata

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { Searchbar } from "react-native-paper";
-import {
-  StyleSheet,
-  View,
-  Text,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
-  console.log(StatusBar.currentHeight);
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>List</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style="auto" />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
