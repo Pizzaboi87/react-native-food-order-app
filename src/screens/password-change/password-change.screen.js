fa9c354caee8ec0354a68e435f9738e45fff2ee4
Peter Weiser <peterweiser87@gmail.com>
2023-04-12 21:29:34 +0300

    weak password handling

--- src/screens/password-change/password-change.screen.js
+++ src/screens/password-change/password-change.screen.js
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
+import { Gif } from "../../helpers/gif-plus-text/gif-plus-text.helper";
+import { DialogWindow } from "../../components/dialog-modal/dialog-modal.component";
+import { updateUserPassword } from "../../services/firebase/firebase-config.service";
 import {
   Container,
   ChangeButton,
   PasswordInput,
   Title,
 } from "./password-change.styles";
-import { Gif } from "../../helpers/gif-plus-text/gif-plus-text.helper";
-import { DialogWindow } from "../../components/dialog-modal/dialog-modal.component";
-import { updateUserPassword } from "../../services/firebase/firebase-config.service";
 
 export const PasswordChangeScreen = ({ navigation }) => {
   const [changeSuccess, setChangeSuccess] = useState(false);
   const [doNotMatch, setDoNotMatch] = useState(false);
   const [changeFail, setChangeFail] = useState(false);
   const [authenticationFail, setAuthenticationFail] = useState(false);
+  const [weakPassword, setWeakPassword] = useState(false);
   const [password, setPassword] = useState({
     currentPW: "",
     newPW1: "",
@@ -28,14 +29,19 @@ export const PasswordChangeScreen = ({ navigation }) => {
   };
 
   const changePassword = () => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
     if (password.newPW1 === password.newPW2) {
-      updateUserPassword(
-        password.currentPW,
-        password.newPW1,
-        setChangeSuccess,
-        setChangeFail,
-        setAuthenticationFail
-      );
+      if (passwordRegex.test(password.newPW1)) {
+        updateUserPassword(
+          password.currentPW,
+          password.newPW1,
+          setChangeSuccess,
+          setChangeFail,
+          setAuthenticationFail
+        );
+      } else {
+        setWeakPassword(true);
+      }
     } else setDoNotMatch(true);
   };
 
@@ -99,6 +105,13 @@ export const PasswordChangeScreen = ({ navigation }) => {
         message="Your current password is not correct."
         visible={authenticationFail}
         setVisible={setAuthenticationFail}
+      />
+
+      <DialogWindow
+        variant="thinking"
+        message={`Your new password is weak. It should:\n- Be at least 8 characters long\n- Contain at least one lowercase letter\n- Contain at least one uppercase letter\n- Contain at least one number`}
+        visible={weakPassword}
+        setVisible={setWeakPassword}
       />
 
       <DialogWindow
