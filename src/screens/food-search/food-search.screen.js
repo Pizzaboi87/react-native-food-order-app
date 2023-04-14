c23fa683b579367a925b2846ec78680949a6f2f7
Peter Weiser <peterweiser87@gmail.com>
2023-04-14 13:57:24 +0300

    added food category selector buttons

--- src/screens/food-search/food-search.screen.js
+++ src/screens/food-search/food-search.screen.js
@@ -1,19 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SafeArea } from "../../helpers/safe-area/safe-area.helper";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Text } from "react-native-paper";
 import { Search } from "../../components/search/search.component";
 import { SearchContainerRestaurant } from "../../components/search/search.styles";
 import { TouchableOpacity } from "react-native";
 import { AvatarImage } from "../../components/user-avatar/user-avatar.component";
 import { UserImageContext } from "../../services/user-image/user-image.context";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { getDataFromDatabase } from "../../services/firebase/firebase-config.service";
+import { LocationContext } from "../../services/location/location.context";
 
 export const FoodSearchScreen = ({ navigation }) => {
   const { useLoadImage } = useContext(UserImageContext);
   const { uid } = useContext(AuthenticationContext);
+  const { keyword } = useContext(LocationContext);
+  const [allMenu, setAllMenu] = useState({});
+  const [allCategories, setAllCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchMenu = async () => {
+      try {
+        const menu = await getDataFromDatabase(
+          "menu",
+          keyword.toLowerCase(),
+          ""
+        );
+
+        const iconSet = new Set();
+        const titleSet = new Set();
+        const iconTitlePairs = [];
+
+        Object.keys(menu).forEach((placeKey) => {
+          const place = menu[placeKey];
+          Object.keys(place).forEach((menuKeys) => {
+            const restaurantMenu = place[menuKeys];
+            Object.keys(restaurantMenu).forEach((categoryKeys) => {
+              const categories = restaurantMenu[categoryKeys];
+              const { icon, title } = categories;
+              if (
+                !iconSet.has(icon) &&
+                icon !== undefined &&
+                title !== undefined
+              ) {
+                iconSet.add(icon);
+                titleSet.add(title);
+                iconTitlePairs.push({ icon, title });
+              }
+            });
+          });
+        });
+        setAllCategories(iconTitlePairs);
+        setAllMenu(menu);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+    fetchMenu();
+  }, [keyword]);
 
   useLoadImage(uid);
+
+  const buttonList = allCategories.map((category) => {
+    return (
+      <Button mode="contained" icon={category.icon} buttonColor="coral">
+        {category.title}
+      </Button>
+    );
+  });
 
   return (
     <SafeArea>
@@ -23,8 +77,16 @@ export const FoodSearchScreen = ({ navigation }) => {
           <AvatarImage size={55} />
         </TouchableOpacity>
       </SearchContainerRestaurant>
-      <View>
-        <Text>Food Search Screen</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: 10,
+          marginTop: 30,
+        }}
+      >
+        {buttonList}
       </View>
     </SafeArea>
   );
